5ccde61fd8902848e673002ca08a8457eee412fd
Uptownboys32 <anggaalfian133@gmail.com>
2021-07-07 03:06:51 +0700

    citrus: Build bluetooth libs

--- device.mk
+++ device.mk
@@ -53,6 +53,11 @@ PRODUCT_PACKAGES += \
     tinymix \
     libaudio-resampler
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
