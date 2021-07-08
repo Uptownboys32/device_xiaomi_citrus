8ff2632eae62efa1397a67af171a0af9c5884fdb
Uptownboys32 <anggaalfian133@gmail.com>
2021-07-09 05:34:58 +0700

    citrus: Import keylayout * from citrus-user 10 QKQ1.200830.002/V12.0.9.0.QJFMIXM release-keys

--- device.mk
+++ device.mk
@@ -138,6 +138,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.bengal
